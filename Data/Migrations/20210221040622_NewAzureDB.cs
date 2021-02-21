55e04ac837d5f60f5968bef9e4e33641cc59be27
YiTzengC <200437546@student.georgianc.on.ca>
2021-02-20 23:06:55 -0500

    add Azure DB

---
+++ Data/Migrations/20210221040622_NewAzureDB.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GeorgianBudgetSaver.Data.Migrations
+{
+    public partial class NewAzureDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
