2c05cc9df2431a56507d469a3fa0632b83cfb3eb
YiTzengC <200437546@student.georgianc.on.ca>
2021-04-05 19:08:40 -0400

    add additional funtionality of retireving data from GC

--- Controllers/CourseProgramsController.cs
+++ Controllers/CourseProgramsController.cs
@@ -8,6 +8,10 @@ using Microsoft.EntityFrameworkCore;
 using GeorgianBudgetSaver.Data;
 using GeorgianBudgetSaver.Models;
 using Microsoft.AspNetCore.Authorization;
+using System.Net;
+using System.IO;
+using System.Text;
+using System.Net.Http;
 
 namespace GeorgianBudgetSaver.Controllers
 {
@@ -156,6 +160,74 @@ namespace GeorgianBudgetSaver.Controllers
         private bool CourseProgramExists(int id)
         {
             return _context.CoursePrograms.Any(e => e.CourseProgramId == id);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> RetrieveFromGCAsync()
+        {
+            //reference to https://html-agility-pack.net/
+            List<CourseProgram> pendingTitle = new List<CourseProgram>();
+            List<string> comingData = new List<string>();
+            //retrieve table info from Georgian College
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://www.georgiancollege.ca/academics/programs/");
+                //curl - X HEAD - i https://www.georgiancollege.ca/academics/programs/
+                //content - encoding = gzip
+                request.Headers.Add(HttpRequestHeader.AcceptEncoding, "gzip,deflate");
+                request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                //get response
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                //convert to string
+                Stream receiveStream = response.GetResponseStream();
+                StreamReader readStream = new StreamReader(receiveStream, Encoding.UTF8);
+                string html = readStream.ReadToEnd();
+                //pass html
+                HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                doc.LoadHtml(html);
+                var tr = doc.DocumentNode.SelectNodes("//tbody/tr");
+                foreach (var el in tr)
+                {
+                    var tds = el.Elements("td");
+                    string code = tds.ElementAt(0).Element("a").InnerText;
+                    string title = tds.ElementAt(1).Element("a").InnerText;
+                    comingData.Add(title);
+                    /*Console.WriteLine($"Code: {code}, Title: {title}");
+*/
+                }
+                //retriev from db
+                List<CourseProgram> dbData = await _context.CoursePrograms.ToListAsync();
+                List<string> dbTitle = new List<string>();
+                foreach (var program in dbData)
+                {
+                    dbTitle.Add(program.Title);
+                }
+                //compare
+                IEnumerable<string> newTitle = comingData.Except(dbTitle);
+
+                if (newTitle.Count() > 0)
+                {
+                    //update db
+                    foreach (string title in newTitle)
+                    {
+                        pendingTitle.Add(
+                            new CourseProgram
+                            {
+                                Title = title
+                            }); ;
+                    }
+                    _context.CoursePrograms.AddRange(pendingTitle);
+                    await _context.SaveChangesAsync();
+                }
+
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine($"Error: {e.Message}");
+            }
+
+
+            return RedirectToAction("Index");
         }
     }
 }
